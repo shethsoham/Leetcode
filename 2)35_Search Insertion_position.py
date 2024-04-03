d70f3c1e609801941a91e968d9fd7fd1dd02a229
Soham Sheth <sohamashishsheth@gmail.com>
2024-04-03 19:10:18 -0400

    Day 1 3 codes of array done

---
+++ 2)35_Search Insertion_position.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0 
+        high = len(nums) -1
+
+        while low<=high :
+            middle = (low + high) // 2
+
+            if nums[middle] == target :
+                return middle
+            
+
+            elif target < nums[middle]:
+                high = middle -1
+
+            else: #target >nums[middle]:
+
+                low = middle + 1
+
+        return low
